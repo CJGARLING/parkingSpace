6ba3d69b4f125c5c20e136b29190824e4d48016b
DIZ001 <ftmurray1993@gmail.com>
2016-10-24 21:24:02 -0400

    Getters and Setters
    Added getters and setters to the parkingSpaces and parkingLot classes

--- parkingSpace/src/parkingSpace/ParkingLot.java
+++ parkingSpace/src/parkingSpace/ParkingLot.java
@@ -1,12 +1,36 @@
-/**
- * 
- */
+
 package parkingSpace;
 
-/**
- * @author Frankie
- *
- */
+
 public class ParkingLot {
+	
+	private String name;
+	private String type; 
+	
+	public ParkingLot(){
+		name = "";
+		type = "";
+	}
+	
+	public ParkingLot(String pLName, String pLType){
+		setName(pLName);
+		setType(pLType);
+	}
+	
+	public void setName(String pLName){
+		name = pLName;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	public void setType(String pLType){
+		type = pLType;
+	}
+	
+	public String getType(){
+		return type;
+	}
 
 }
